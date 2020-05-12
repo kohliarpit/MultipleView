2d43ab18aabfec43476596636978dbf72dac7fe9
Arpit Kohli <arpit.kohli@nagarro.com>
2020-05-12 11:01:10 +0530

    Initial Commit

---
+++ Multiple-View-ProductTests/Multiple_View_ProductTests.swift
@@ -0,0 +1,34 @@
+//
+//  Multiple_View_ProductTests.swift
+//  Multiple-View-ProductTests
+//
+//  Created by Arpit Kohli on 12/05/20.
+//  Copyright © 2020 Arpit Kohli. All rights reserved.
+//
+
+import XCTest
+@testable import Multiple_View_Product
+
+class Multiple_View_ProductTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
